18709bfa24f7e4dac752e996b888d555477ec955
rifqi856 <rifqithufail1208@gmail.com>
2025-06-15 10:11:56 +0700

    Update Materi

---
+++ function.js
@@ -0,0 +1,19 @@
+//FUNCTION
+
+function addName(){
+    return 'Rifqi Thufail'  // kalo tdk di retrun maka akan jadi undifained
+}
+
+alert(addName())
+//kalo kita memanggil alert(addName) maka itu memanggil semua yg ada di dlm addname
+//tapi kalo kita tambah buka - tutup kurung bisa maka hanya akan memanggil yg di retrunnya saja
+
+function addName1(firstName, lastName){
+    console.log(firstName, lastName)
+    console.log('Nama :', firstName)
+    console.log('Nama Marga :', lastName)
+}
+
+
+alert(addName1('Ahmad', 'Syafiq'))
+
