154727887c659326d87915ef91e1539490994ff0
rifqi856 <rifqithufail1208@gmail.com>
2025-06-15 11:37:38 +0700

    perbarui materi 1

--- function.js
+++ function.js
@@ -10,6 +10,7 @@ alert(addName())
 
 function addName1(firstName, lastName){
     console.log(firstName, lastName)
+    console.log(firstName + lastName)
     console.log('Nama :', firstName)
     console.log('Nama Marga :', lastName)
 }
@@ -20,7 +21,65 @@ alert(addName1('Ahmad', 'Syafiq'))
 
 function addName2(){
     console.log(arguments)
+    console.log('nama',arguments[0])
+    console.log('marga',arguments[1])
+
 }
 
 
 alert(addName2('Ahmad', 'Syafiq'))
+
+
+function addName3(){
+    for(let i = 0; i < arguments.length; i++){
+        console.log('nama lengkap :', arguments[i])
+    }
+
+}
+
+
+alert(addName3('Ahmad', 'Syafiq'))
+
+
+//cara ke 2 di dlm variable
+
+const fullName = function(name1, name2){
+    console.log(name1, name2)
+}
+
+fullName('zaki', 'ahmad')
+
+
+const sum = new Function('x', 'y', 'return x+y')
+console.log(sum(1, 2))
+
+//cara1 keyword new function
+const sum1 = new Function('console.log("ini adalah sum")')
+sum1()
+
+
+// cara2 standar function
+
+function sum2(x, y){
+    console.log(x+y)
+}
+sum2(4, 6)
+
+//cara3 arrow function
+const sum3 = (x,y) => {
+    console.log(x+y)
+}
+
+sum3(4,7)
+
+
+//hoisting
+
+console.log(data)    //kalo di panggil dulu baru pengkondisian bakal undifained
+var data = 1   
+
+
+sum2(4,9)     
+function sum2(x, y){
+    console.log(x+y)
+}
